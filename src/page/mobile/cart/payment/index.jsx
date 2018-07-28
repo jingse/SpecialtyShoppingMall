b8bf7a0192b03310323b229bd3e174346afcde4d
vngcbupt <lulifeng_v@163.com>
2018-07-28 15:26:20 +0800

    pay shouldpay

--- src/page/mobile/cart/payment/index.jsx
+++ src/page/mobile/cart/payment/index.jsx
@@ -339,11 +339,11 @@ class Payment extends React.Component {
             "totalMoney":this.state.priceResult.totalMoney.toFixed(2),
             "promotionAmount":this.state.priceResult.promotionMoney.toFixed(2),
             "shipFee":this.state.shipFee.toFixed(2),
-            "shouldPayMoney":(this.state.priceResult.totalMoney - this.state.priceResult.promotionMoney + this.state.shipFee - this.state.couponSub - this.state.balancenum).toFixed(2),
+            "payMoney":(this.state.priceResult.totalMoney - this.state.priceResult.promotionMoney + this.state.shipFee - this.state.couponSub - this.state.balancenum).toFixed(2),
             "couponMoney":this.state.couponSub.toFixed(2),
             // "balanceMoney":balance.toFixed(2),
             "balanceMoney":(this.state.balancenum).toFixed(2),
-            "payMoney":(this.state.priceResult.totalMoney - this.state.priceResult.promotionMoney + this.state.shipFee).toFixed(2),
+            "shouldPayMoney":(this.state.priceResult.totalMoney - this.state.priceResult.promotionMoney + this.state.shipFee).toFixed(2),
 
             // "receiverRmark":"请送到我家",
             // "receiverName":"王五",
